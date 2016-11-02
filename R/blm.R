4bc32649b22d76a81280a9a8c70f14e5aea7c99b
Thomas Mailund <mailund@birc.au.dk>
2016-11-02 16:38:04 +0100

    Setting up package skeleton

---
+++ R/blm.R
@@ -0,0 +1,14 @@
+
+#' Bayesian linear model.
+#'
+#' Fits a model, given as a formula, optionally with data provided through the "..." parameter.
+#'
+#' @param model   A formula describing the model.
+#' @param ...     Additional data, for example a data frame. Feel free to add other options.
+#'
+#' @return A fitted model.
+#' @export
+blm <- function(model, ...) {
+  # implement your function here...
+}
+
