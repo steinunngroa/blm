0cfe4a5f336f6548a80620cae4e0ca53b5ac7db6
steinunngroa <steinunngroa@gmail.com>
2016-12-13 12:33:46 +0100

    Made in class 13/12

--- R/blm.R
+++ R/blm.R
@@ -8,7 +8,40 @@
 #'
 #' @return A fitted model.
 #' @export
-blm <- function(model, ...) {
-  # implement your function here...
+#'
+#' ## Bayesian linear regresssion
+blm <- function(model, alpha, beta, data, ...) {
+  phi <- model.matrix(model, data)
+  S <- solve(diag(alpha, nrow = ncol(phi)) + beta*t(phi) %*% phi)
+  mxy <- beta * S %*% t(phi) %*% model.response(model.frame(model, data))
+  structure(list(sigma = S, mean = mxy, data = data, model=model), class="blm")
 }
 
+#test= blm(y ~ x+I(x^2), 1, 1, data.frame(x=x, y=x))
+
+
+predict.blm <- function(blmModel) {
+  phi = model.matrix(blmModel$model, blmModel$data)
+  m = blmModel$mean
+  S = blmModel$sigma
+
+  apply(phi, 1, function(row) t(m)%*%row)
+}
+
+#predict(test)
+
+
+#
+#
+# ```r
+# library(covr)
+# package_coverage()
+# ```
+#
+# or get a window with the report using
+#
+# ```r
+# library(covr)
+# shine(package_coverage())
+# ```
+#
